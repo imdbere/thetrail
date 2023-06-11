2472711ea76fac43416520cfd3295b21c7e1e115
Aaron <aaron@coinpanion.com>
2023-06-11 22:11:38 +0200

    added sicily car code

--- src/app/crumb/validCrumbs.ts
+++ src/app/crumb/validCrumbs.ts
@@ -1,5 +1,5 @@
 export const validCrumbs = [
-  { code: "1234", location: "Rental car in Sicily" },
+  { code: "x5j8", location: "Rental car in Sicily" },
   { code: "crts43", location: "Rental car in Vienna" },
 ];
 
