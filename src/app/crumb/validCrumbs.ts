e2dcf30b47db8eac200c54fe3907ab8aaeabbfbf
Aaron <aaron@coinpanion.com>
2023-06-11 20:13:31 +0200

    added check if crumb exists

---
+++ src/app/crumb/validCrumbs.ts
@@ -0,0 +1,6 @@
+export const validCrumbs = [
+  { code: "1234", location: "Rental car in Sicily" },
+  { code: "crts43", location: "Rental car in Vienna" },
+];
+
+export type Crumb = (typeof validCrumbs)[0];
